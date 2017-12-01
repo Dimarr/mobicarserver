7f05aedd90a9116818878c5e05ed72eb1106479f
maorsom@gmail.com <Oran27515>
2017-12-01 09:59:12 +0300

    Testting

--- src/main/java/MyToken.java
+++ src/main/java/MyToken.java
@@ -2,33 +2,31 @@
  * Created by РумянцевДмитрий on 27.11.2017.
  */
 import com.twilio.jwt.accesstoken.AccessToken;
-import com.twilio.jwt.accesstoken.ChatGrant;
+import com.twilio.jwt.accesstoken.VoiceGrant;
 
 public class MyToken {
             // Required for all types of tokens
-            static String twilioAccountSid = "ACxxxxxxxxxxxx";
+            static String twilioAccountSid = "AC412c3c70ad5d2d4da6b7b5e35feac84d";
             static String twilioApiKey = "SKxxxxxxxxxxxx";
             static String twilioApiSecret = "xxxxxxxxxxxxxx";
 
             // Required for Voice
-            static String outgoingApplicationSid = "APxxxxxxxxxxxx";
-            static String identity = "user";
+            static String outgoingApplicationSid = "mobicarApp";
+            static String identity = "mobicar";
 
             // Create Voice grant
             public MyToken() { }
             public static String newToken() {
-                String serviceSid = "ISxxxxxxxxxxxx";
-                String deviceId = "someiosdevice";
-                String identity = "user@example.com";
-                String appName = "HipFlowSlackDockRC";
-                String endpointId = appName + ":" + identity + ":" + deviceId;
+                VoiceGrant grant = new VoiceGrant();
+                grant.setOutgoingApplicationSid(outgoingApplicationSid);
 
-                ChatGrant grant = new ChatGrant();
-                grant.setEndpointId(endpointId);
-                grant.setServiceSid(serviceSid);
+                // Create access token
+                AccessToken token = new AccessToken.Builder(
+                twilioAccountSid,
+                twilioApiKey,
+                twilioApiSecret
+                ).identity(identity).grant(grant).build();
 
-                AccessToken token = new AccessToken.Builder(twilioAccountSid, twilioApiKey, twilioApiSecret)
-                        .identity(identity).grant(grant).build();
                 return token.toJwt();
                 //System.out.println(token.toJwt());
     }
