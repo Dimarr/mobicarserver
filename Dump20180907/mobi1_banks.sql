f1f276c503b2e587e44877ba908fe1663c6704ea
Румянцев Дмитрий <drumiantsev@sberbank-tele.com>
2018-09-07 16:34:45 +0300

    0709_
    database

---
+++ Dump20180907/mobi1_banks.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE  IF NOT EXISTS `mobi1` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `mobi1`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+--
+-- Host: mobi1.caqm9reiespt.us-east-1.rds.amazonaws.com    Database: mobi1
+-- ------------------------------------------------------
+-- Server version	5.6.27-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `banks`
+--
+
+DROP TABLE IF EXISTS `banks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `banks` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `banks`
+--
+
+LOCK TABLES `banks` WRITE;
+/*!40000 ALTER TABLE `banks` DISABLE KEYS */;
+INSERT INTO `banks` VALUES (4,'Bank Yahav for Government Employees Ltd'),(10,'Bank Leumi Le-Israel B.M'),(11,'Israel Discount Bank Ltd'),(12,'Bank Hapoalim B.M'),(13,'Union Bank of Israel Ltd'),(14,'Bank Otsar Ha-hayal Ltd'),(17,'Mercantile Discount Bank Ltd'),(20,'Mizrahi Tefahot Bank Ltd'),(22,'Citibank N.A'),(23,'HSBC Bank plc'),(25,'BNP Paribas Israel'),(26,'UBank Ltd'),(27,'Barclays Bank PLC'),(30,'Trade Bank Ltd- a provisional liquidator was '),(31,'The First International Bank of Israel Ltd'),(34,'Arab Israel Bank Ltd'),(39,'State Bank Of India'),(46,'Bank Massad Ltd'),(50,'Bank Clearing Center Ltd'),(52,'Poaley Agudat Israel Bank Ltd'),(54,'Bank of Jerusalem Ltd'),(59,'Automated Banking Services Ltd'),(65,'Hesech- Kupat Hisachon Lechinuch Ltd'),(68,'Dexia Israel Bank Ltd');
+/*!40000 ALTER TABLE `banks` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-07 16:33:24
